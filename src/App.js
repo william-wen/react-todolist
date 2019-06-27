4406f1c708a77f67a3be7bb7b4eb951e3602ddda
William Wen <wenwi@utschools.ca>
2019-06-27 21:31:37 +0800

    Programmed Add Function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: '',
+      listItems: [],
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value,
+    })
+  }
+
+  handleClick = (value) => {
+    let arrayItem = <div> {value} </div>;
+    this.setState({
+      listItems: this.state.listItems.concat(arrayItem)
+    });
+  }
+
+  render () {
+    const { value, listItems } = this.state;
+    return (
+      <div className="App">
+        <div className="inputAndButton">
+          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+          <button onClick={() => this.handleClick(value)}> Add </button>
+        </div>
+        {listItems}
+      </div>
+    );
+  }
 }
 
 export default App;

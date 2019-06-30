ca78ee4ef8ebb610434b6626133b76344d7281a1
William Wen <wenwi@utschools.ca>
2019-06-30 11:05:09 +0800

    Updates

---
+++ precache-manifest.83575d81f15ed46f6a05cd8863892619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84390320c36aef8c49335c6940f0a90f",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "eb234dd6ed3dea3db94c",
+    "url": "/react-todolist/static/css/2.39bbabe8.chunk.css"
+  },
+  {
+    "revision": "d439685719950b2d89e5",
+    "url": "/react-todolist/static/css/main.8621d52b.chunk.css"
+  },
+  {
+    "revision": "eb234dd6ed3dea3db94c",
+    "url": "/react-todolist/static/js/2.60096290.chunk.js"
+  },
+  {
+    "revision": "d439685719950b2d89e5",
+    "url": "/react-todolist/static/js/main.d2a32eed.chunk.js"
+  },
+  {
+    "revision": "dd2a0c730e02a59d4852",
+    "url": "/react-todolist/static/js/runtime~main.8b2e420c.js"
+  }
+]);
